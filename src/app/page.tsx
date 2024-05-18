509a416f527fe932078e23bb879b3b351356f8f4
Atanu Pramanik <atanupramanik506@gmail.com>
2024-05-18 20:26:14 +0530

    update3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+
+  const onSignup = async () => {
+    try {
+
+
+      const response = await axios.post("/api/users/signup/");
+    } catch (error: any) {
+      console.log("signup failed", error.message)
+      
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+    
+    <main className = "flex min-h-screen flex-col items-center justify-between p-24" >
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -109,11 +124,14 @@ export default function Home() {
           </p>
         </a>
       </div>
-      <div className=" p-4 bg-orange-400">
-        <Link href="/signup">
-          SignUp
-        </Link>
+      <div>
+        <button
+        onClick={onSignup}
+        className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"  >
+          SingUp
+        </button>
       </div>
+      
     </main>
   );
 }
