253f093562cc6613a2576d393e76b4ef2994c572
Atanu Pramanik <atanupramanik506@gmail.com>
2024-05-18 20:14:01 +0530

    update2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -107,6 +108,11 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+      </div>
+      <div className=" p-4 bg-orange-400">
+        <Link href="/signup">
+          SignUp
+        </Link>
       </div>
     </main>
   );
